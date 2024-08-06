35168df9c81313673c7107ecfe4e22d29bb8e493
A12ise <sorrawit456flookza@gmail.com>
2024-08-06 21:57:59 +0700

    Update product.go

--- week5/backend/entity/product.go
+++ week5/backend/entity/product.go
@@ -4,14 +4,14 @@ import "gorm.io/gorm"
 
 type Product struct {
 	gorm.Model
-	Product_id   string `json:"product_id"`
+	Product_id   string 
 
-	Product_name string	`json:"product_name"`
+	Product_name string	
 
-	Category_id  string	`json:"category_id"`
+	Category_id  string	
 
-	Employee_id  string	`json:"employee_id"`
+	Employee_id  string	
 
-	Order_Product []Order_Product `gorm:"foreignKey:product_id"`
+	Order_Product []Order_Product `gorm:"foreignKey:Product_id"`
 
 }
