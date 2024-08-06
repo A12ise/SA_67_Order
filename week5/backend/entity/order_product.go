05fc92aec6c0fee5efbaa6e7693f0f5e8c9b5f35
A12ise <sorrawit456flookza@gmail.com>
2024-08-06 21:57:58 +0700

    Update order_product.go

--- week5/backend/entity/order_product.go
+++ week5/backend/entity/order_product.go
@@ -1,12 +1,13 @@
 package entity
 
-import (
-	"time"
-
-	"gorm.io/gorm"
-)
+import "gorm.io/gorm"
 
 type Order_Product struct {
 	gorm.Model
-	
+	Order_id    int   
+	Product_id  string
+	Category_id string
+	Quantity   int 
+	Order      Order  `gorm:"foreignKey:Order_id"`
+	Product    Product `gorm:"foreignKey:Product_id"`
 }
